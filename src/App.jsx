9ec9790350e09863011195bcbffab4585fb042f4
João Vitor <devjohncb@gmail.com>
2024-02-04 19:18:24 -0300

    initial typescale structure created

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,23 @@ import { Fragment } from 'react';
 function App() {
   return (
     <Fragment>
-      <h1 className="text-4xl font-bold text-indigo-700">Palavra de Honra</h1>
+      <span className="title">Cyber Currency Center</span>
+      <h1>Discover the most famous cyber economy systems</h1>
+      <h2>Explore the Digital Market</h2>
+      <h3>Cryptocurrencies in the Digital Financial Market</h3>
+      <h4>Cryptocurrencies</h4>
+      <h5>Payment Method and International Fund Transfer</h5>
+      <h6>
+        Discover the world of Cryptocurrencies, Blockchain and NFTs in this
+        application, learn everything about the digital economy.
+      </h6>
+      <a href="">Home</a>
+      <p>
+        A cryptocurrency is a form of digital currency that uses cryptography
+        for secure transactions, operating on a decentralized network called
+        blockchain. Examples include Bitcoin and Ethereum, providing security
+        and transparency.
+      </p>
     </Fragment>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      first: 'Outfit',
+      second: 'IBM Plex Mono',
+    },
+    colors: {
+      raisin: '#131222',
+      space: '#171627',
+      cadet: '#1E1D32',
+      turquoise: '#04D6C5',
+      alice: '#E3EDF7',
+    },
     extend: {},
   },
   plugins: [],
