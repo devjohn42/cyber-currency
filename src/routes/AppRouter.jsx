3cfee28ad584f4c3924765f895c26a3a2099e32f
João Vitor <devjohncb@gmail.com>
2024-02-18 23:13:05 -0300

    fixing a wrong path

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import HomePage from '../Pages/HomePage';
+import HomePage from '../pages/HomePage';
 import CryptoApp from '../pages/CryptoStats/CryptoApp';
 import CoinInfo from '../pages/CryptoStats/components/Coins/CoinInfo';
 import Error from '../pages/Error';
